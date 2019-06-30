12854856217ff1a458a8c677ed974f6d4406ee58
Ferenc Hechler <github@fh.anderemails.de>
2019-06-30 13:12:03 +0200

    init brain.

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 tt.test()
+nn.initFCNN(5, [6, 6], 3)
 let Liste = [1.25, 2.25]
 basic.showNumber(tt.sumvec(Liste))
 basic.showString("Hello!")
+Liste = nn.predict(Liste)
+basic.showNumber(tt.sumvec(Liste))
